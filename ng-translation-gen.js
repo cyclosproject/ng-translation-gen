0b370b7a3cf4ee43954e7fd7b42f8464c433bc7e
Luis Fernando Planella Gonzalez <lfpg.dev@gmail.com>
2021-03-04 15:00:21 -0300

    Refactored how the translation is generated
    Fixes #16

--- ng-translation-gen.js
+++ ng-translation-gen.js
@@ -21,6 +21,25 @@ const METHOD_REG_EXP = /[\. | \_ | \-]\w{1}/g;
 // E.g: AccessMesagges -> access-messages.ts
 const FILE_REG_EXP = /[A-Z]{1}/g;
 
+const RESERVED = [
+  'abstract', 'arguments', 'await', 'boolean',
+  'break', 'byte', 'case', 'catch',
+  'char', 'class', 'const', 'continue',
+  'debugger', 'default', 'delete', 'do',
+  'double', 'else', 'enum', 'eval',
+  'export', 'extends', 'false', 'final',
+  'finally', 'float', 'for', 'function',
+  'goto', 'if', 'implements', 'import',
+  'in', 'instanceof', 'int', 'interface',
+  'let', 'long', 'native', 'new',
+  'null', 'package', 'private', 'protected',
+  'public', 'return', 'short', 'static',
+  'super', 'switch', 'synchronized', 'this',
+  'throw', 'throws', 'transient', 'true',
+  'try', 'typeof', 'var', 'void',
+  'volatile', 'while', 'with', 'yield'
+]
+
 /**
  * Main generation function
  */
@@ -59,10 +78,10 @@ function ngTranslationGen(options) {
     if (baseName.toLowerCase().endsWith('.json')) {
       baseName = baseName.substring(0, baseName.length - 5);
     }
-    const fileName = baseName + '.json';
+    const filename = baseName + '.json';
     try {
       // Read the file content
-      const fullFilename = path.join(options.input, fileName);
+      const fullFilename = path.join(options.input, filename);
       const content = fs.readFileSync(fullFilename);
       let translations = JSON.parse(content, "utf-8");
 
@@ -72,7 +91,7 @@ function ngTranslationGen(options) {
 
       // Set additional properties for the root model
       model.defaultLocale = defaultLocale;
-      model.defaultFilename = fileName;
+      model.defaultFilename = filename;
       model.defaultHash = contentHash(fullFilename);
       model.locales = locales;
       // Make a copy of each additional locale model
@@ -85,17 +104,21 @@ function ngTranslationGen(options) {
         l.hash = contentHash(localeFile);
       });
       model.separator = options.separator;
+      model.metaClassName = className + 'Meta';
+      model.filename = getTSFilename(className);
+      model.metaFilename = getTSFilename(model.metaClassName);
 
-      // Render the template
-      const code = templates.messages(model);
+      // Write the interface
+      let outFile = path.join(options.output, model.filename + ".ts");
+      fs.writeFileSync(outFile, templates.messages(model), "utf-8");
+      console.info('Wrote ' + outFile);
 
-      // write the generated class
-      let tsName = getTSFilename(className);
-      let outFile = path.join(options.output, tsName + ".ts");
-      fs.writeFileSync(outFile, code, "utf-8");
+      // Write the metadata
+      outFile = path.join(options.output, model.metaFilename + ".ts");
+      fs.writeFileSync(outFile, templates.meta(model), "utf-8");
       console.info('Wrote ' + outFile);
     } catch (error) {
-      const e1 = new Error(`Generation aborted, error processing file: ${fileName} (${error}).`);
+      const e1 = new Error(`Generation aborted, error processing file: ${filename} (${error}).`);
       e1.stack = error.stack;
       throw e1;
     }
@@ -104,10 +127,10 @@ function ngTranslationGen(options) {
   fs.copyFileSync(path.join(__dirname, 'static', "translations.ts"), path.join(options.output, "translations.ts"));
 }
 
-function contentHash(fileName) {
+function contentHash(filename) {
   let content;
   try {
-    content = fs.readFileSync(fileName, {
+    content = fs.readFileSync(filename, {
       encoding: 'utf-8'
     });
   } catch (e) {
@@ -124,7 +147,9 @@ function getTemplateModel(translations, className, path, baseName, argumentType)
   let model = {
     className: className,
     baseName: baseName,
-    direct: [],
+    simple: [],
+    named: [],
+    positional: [],
     nested: [],
     hasNested: false,
     path: path,
@@ -136,8 +161,11 @@ function getTemplateModel(translations, className, path, baseName, argumentType)
   const directKeys = allKeys.filter(k => typeof translations[k] === 'string');
   if (directKeys.length > 0) {
     for (const key of directKeys) {
+      let positionalKeys = [];
+      let namedKeys = [];
       let positionalArgs = {};
       let namedArgs = {};
+      let mapping = undefined;
       const value = translations[key];
       // each match is of the form {argName} or {a_number}
       value.replace(ARGS_REG_EXP, (match) => {
@@ -146,68 +174,93 @@ function getTemplateModel(translations, className, path, baseName, argumentType)
         // if it is of the form {a_number} then the resulting param name will be
         // prefixed with 'arg'
         let positional = match.match(/\{\d+\}/);
+        const paramKey = paramName;
         if (positional) {
-          paramName = "arg" + paramName;
+          paramName = "$" + paramName;
+        } else {
+          paramName = getValidIdentifier(paramName);
         }
         let param = {
+          positional,
+          key: paramKey,
           name: paramName,
-          identifier: getValidIdentifier(paramName),
-          type: argumentType,
-          lastTxArg: false
+          type: argumentType
         };
         if (positional) {
-          positionalArgs[paramName] = param;
+          if (!positionalKeys.includes(paramName)) {
+            positionalKeys.push(paramName)
+            positionalArgs[paramName] = param;
+          }
         } else {
-          namedArgs[paramName] = param;
+          if (!namedKeys.includes(paramName)) {
+            namedKeys.push(paramName);
+            namedArgs[paramName] = param;
+          }
         }
       });
 
-      // Get both positional and named ards
-      let positionalKeys = Object.keys(positionalArgs);
-      let namedKeys = Object.keys(namedArgs);
-
       // If there's a single named arg, we handle it as positional
-      if (namedKeys.length === 1) {
-        let key = namedKeys[0];
-        let arg = namedArgs[key];
+      const namedAsPositional = namedKeys.length === 1 && positionalKeys.length === 0;
+      if (namedAsPositional) {
+        let paramKey = namedKeys[0];
+        let arg = namedArgs[paramKey];
         arg.positional = true;
-        positionalKeys.push(key);
-        positionalArgs[key] = arg;
+        positionalKeys.push(paramKey);
+        positionalArgs[paramKey] = arg;
         namedKeys = [];
         namedArgs = {};
       }
 
-      // Set the full identifier
-      for (let key of positionalKeys) {
-        let arg = positionalArgs[key];
-        arg.fullIdentifier = arg.identifier;
-      }
-      for (let key of namedKeys) {
-        let arg = namedArgs[key];
-        arg.fullIdentifier = '$.' + arg.identifier;
+      // If there's a mix of positional and named keys, handle all positional arguments as named
+      if (namedKeys.length > 0 && positionalArgs.length > 0) {
+        positionalArgs.forEach(arg => {
+          const paramKey = `arg${arg}`;
+          namedKeys.push(paramKey);
+          namedArgs[paramKey] = arg;
+          arg.positional = false;
+        })
+        positionalKeys = [];
+        positionalArgs = {};
       }
 
-      // First add all positional args, in order
-      let args = Object.keys(positionalArgs).sort().map(name => positionalArgs[name]);
+      // Compute each argument
+      let args = [];
       if (namedKeys.length > 0) {
+        namedKeys.forEach(k => {
+          const mappedTo = namedArgs[k].key;
+          if (k !== mappedTo) {
+            if (!mapping) {
+              mapping = {};
+            }
+            mapping[k] = mappedTo;
+          };
+        });
         args.push({
           name: '$',
           type: `{${namedKeys.map(n => n + ': ' + argumentType).join(', ')}}`
         });
-        let lastKey = namedKeys[namedKeys.length - 1];
-        namedArgs[lastKey].lastTxArg = true;
-      } else if (positionalArgs.length > 0) {
-        positionalArgs[positionalArgs.length - 1].lastTxArg = true;
+      } else if (positionalKeys.length > 0) {
+        if (namedAsPositional) {
+          const first = positionalKeys[0];
+          mapping = positionalArgs[first].key;
+        }
+        positionalKeys.forEach(k => {
+          const arg = positionalArgs[k];
+          args.push({
+            name: arg.name,
+            type: argumentType
+          });
+        });
       }
 
       // Add the direct property model
-      model.direct.push({
+      const arr = args.length === 0 ? model.simple : positionalKeys.length > 0 ? model.positional : model.named;
+      arr.push({
         name: getValidIdentifier(key),
+        key,
         defaultValue: value.replace(/\n/g, '\\n').replace(/\*\//g, "* /"),
-        hasArgs: args.length > 0,
         args: args,
-        txArgs: [...positionalKeys.map(k => positionalArgs[k]), ...namedKeys.map(k => namedArgs[k])],
-        key: key
+        mapping: mapping ? JSON.stringify(mapping) : undefined
       });
     }
   }
@@ -219,7 +272,7 @@ function getTemplateModel(translations, className, path, baseName, argumentType)
     for (const key of nestedKeys) {
       const value = translations[key];
       const nestedPath = (path ? path + '.' + key : key);
-      let nestedClass = getValidIdentifier(key);
+      let nestedClass = getValidIdentifier(key, false);
       nestedClass = nestedClass.charAt(0).toUpperCase() + nestedClass.substring(1);
       const nested = getTemplateModel(value, className + '$' + nestedClass, nestedPath, null, argumentType);
       nested.property = key;
@@ -227,7 +280,7 @@ function getTemplateModel(translations, className, path, baseName, argumentType)
       model.nested.push(nested);
     }
   }
-
+  model.withArgs = [...(model.named || []), ...(model.positional || [])];
   return model;
 }
 
@@ -255,14 +308,13 @@ function getTSFilename(className) {
  * @param name E.g. a trasnslation key of the form word1.word2.
  * @returns a camel-cased name of the form word1Word2.
  */
-function getValidIdentifier(name) {
+function getValidIdentifier(name, checkReserved = true) {
   // more than one char in upper case is lower cased
   name = name.replace(UPPER_CASE_REG_EXP, (match) => match.length == 1 ? match : match.toLowerCase());
   // each char after a '.', '_' or '-' is upper cased
   name = name.replace(METHOD_REG_EXP, (match) => match.substring(1).toUpperCase());
-  if (name === 'initialize') {
-    // Initialize is a special method - cannot be the name of a key
-    name = 'initialize$';
+  if (checkReserved && RESERVED.includes(name)) {
+    name = `'${name}'`;
   }
   return name;
 }
--- static/translations.ts
+++ static/translations.ts
@@ -1,4 +1,5 @@
 /* tslint:disable */
+import { BehaviorSubject } from 'rxjs';
 
 /**
  * Container for translation values
@@ -8,38 +9,142 @@ export type TranslationValues = { [key: string]: string | TranslationValues };
 /**
  * Provides access to translation values
  */
-export class Translations {
+export class Translations<I extends Object> {
   private ARGS_RE: RegExp = /\{\w+\}/g;
-  private NUMERICAL_ARGS_RE: RegExp = /\d+/;
-
+  private _obj: any;
   private _defaultValues: TranslationValues = {};
   private _values: TranslationValues = {};
   private _keys: string[] = [];
+  private _nested = new Map<string, Translations<any>>();
 
-  constructor(private _path?: string) {
+  /** Indicates whether the translation values have been initialized **/
+  initialized$ = new BehaviorSubject(false);
+
+  constructor(public path?: string) {
+    this._obj = {
+      initialized$: this.initialized$,
+      $initialize: (translations: { [key: string]: any }) => this.initialize(translations),
+      $defaults: (defaults: { [key: string]: any }) => this.defaultValues = defaults
+    };
+  }
+
+  /**
+   * Returns the object that can be used to access the translations
+   */
+  object(): I {
+    return this._obj as I;
   }
 
   /**
    * Initializes the translation values
    * @param values The translations values
    */
-  initialize(values: Object) {
-    this._values = (values || {}) as TranslationValues;
+  initialize(values: any) {
+    values = values || {};
+    this._values = {} as TranslationValues;
     this._keys.length = 0;
 
-    for (const prop of Object.keys(this._values)) {
-      const value = this._values[prop];
-      if (typeof value === 'string') {
-        this._keys.push(prop);
+    for (const prop of Object.keys(values)) {
+      const valid = this.validProperty(prop);
+      if (valid) {
+        this._values[valid] = values[prop];
+        const value = this._values[valid];
+        if (typeof value === 'string') {
+          this._keys.push(valid);
+        } else if (typeof value === 'object') {
+          const tx = this._nested.get(valid);
+          if (tx) {
+            tx.initialize(value);
+          }
+        }
       }
     }
+
+    this.initialized$.next(true);
+  }
+
+  /**
+   * Registers a simple property (no arguments)
+   */
+  simple(...properties: string[]) {
+    const tx = this;
+    (properties || []).forEach(p => {
+      Object.defineProperty(this._obj, p, {
+        enumerable: true,
+        get: () => tx.get(p)
+      });
+    });
+  }
+
+  /**
+   * Registers a property with a single argument (direct arg function)
+   */
+  named(property: string, mapping?: { [key: string]: string }) {
+    const tx = this;
+    this._obj[property] = (params: { [key: string]: any }) => {
+      // We might need a remapping between the property and the actual key (example, if the key has a dot or dash)
+      let args = params;
+      if (params && mapping) {
+        // We'll need to mutate the params
+        args = { ...args };
+        const key = mapping[property] || property;
+        if (key && key !== property) {
+          args[property] = args[key];
+        }
+      }
+      return tx.get(property, args);
+    };
+  }
+
+  /**
+   * Registers a property with multiple arguments (receives an object)
+   */
+  positional(property: string, mapping?: string) {
+    const tx = this;
+    this._obj[property] = (...params: any[]) => {
+      const args: { [key: string]: any } = {};
+      params = params || [];
+      if (mapping) {
+        // The only possible case for a mapping is a single positiona param with a key
+        args[mapping] = params;
+      } else {
+        for (let i = 0; i < params.length; i++) {
+          args[String(i)] = params[i];
+        }
+      }
+      return tx.get(property, args);
+    };
+  }
+
+  /**
+   * Registers a nested translations object
+   */
+  nested(...txs: Translations<any>[]) {
+    txs.forEach(tx => {
+      const p = this.getValidIdentifier(tx.path);
+      this._nested.set(p, tx);
+      Object.defineProperty(this._obj, p, {
+        enumerable: true,
+        get: () => {
+          return tx.object();
+        }
+      });
+    });
   }
 
   /**
    * Sets the default values, which work as a fallback for missing keys
    */
-  set defaultValues(defaultValues: Object) {
-    this._defaultValues = (defaultValues || {}) as TranslationValues;
+  set defaultValues(defaultValues: any) {
+    defaultValues = defaultValues || {};
+    this._defaultValues = {} as TranslationValues;
+    for (const key of Object.keys(defaultValues)) {
+      const valid = this.validProperty(key);
+      if (valid) {
+        this._defaultValues[valid] = defaultValues[key];
+      }
+    }
+    this._nested.forEach((tx, property) => tx.defaultValues = defaultValues[property])
   }
 
   /**
@@ -52,7 +157,7 @@ export class Translations {
   /**
    * Returns the value associated to the given key without argument interpolation
    */
-  rawValue(key: string): string {
+  rawValue(key: string): string | undefined {
     const value = this._values[key] || this._defaultValues[key];
     return typeof value === 'string' ? value : undefined;
   }
@@ -62,19 +167,34 @@ export class Translations {
    * @param key The message key
    * @param params The parameters
    */
-  get(key: string, params: Object = {}): string {
+  get(key: string, params: { [key: string]: any } = {}): string {
     const value = this.rawValue(key);
     if (value == null) {
-      return `???${this._path ? this._path + '.' + key : key}???`;
+      return `???${this.path ? this.path + '.' + key : key}???`;
     } else {
       return value.replace(this.ARGS_RE, (substring: string) => {
-        let paramName = substring.substring(1, substring.length - 1);
-        // e.g in case of {0} the corresponding property is arg0
-        if (this.NUMERICAL_ARGS_RE.test(paramName)) {
-          paramName = 'arg' + paramName;
-        }
+        const paramName = substring.substring(1, substring.length - 1);
         return String(params[paramName]);
       });
     }
+  }
+
+  // Both regexp are used to get the method name from the translation key.
+  // E.g: error.Invalid -> errorInvalid
+  private static UPPER_CASE_REG_EXP = /[A-Z]+/g;
+  private static METHOD_REG_EXP = /[\. | \_ | \-]\w{1}/g;
+
+  private getValidIdentifier(name: any) {
+    name = String(name);
+    // more than one char in upper case is lower cased
+    name = name.replace(Translations.UPPER_CASE_REG_EXP, (match: string) => match.length == 1 ? match : match.toLowerCase());
+    // each char after a '.', '_' or '-' is upper cased
+    name = name.replace(Translations.METHOD_REG_EXP, (match: string) => match.substring(1).toUpperCase());
+    return name;
+  }
+
+  private validProperty(name: string) {
+    name = this.getValidIdentifier(name);
+    return this._obj.hasOwnProperty(name) ? name : null;
   }
 }
